0230ed273ab498e67c6df3884c558c00e2e7077d
Rachel Tirkey <racheltirkey.in@gmail.com>
2024-07-24 11:54:12 +0530

    added logic to the page

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,27 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digital Clock</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+function updateClock(){
 
-    <div class="container">
-        <div class="card">
-            <h1>Digital Clock</h1>
-            <div class="clock">
-                <span class="time-element" id ="hours">00</span>
-                <span class="colon-element">:</span>
-                <span class="time-element" id="minutes">00</span>
-                <span class="colon-element">:</span>
-                <span class="time-element" id="seconds">00</span>
-            </div>
-        </div>   
-    </div>
-    
-<script src="script.js"></script>   
-</body>
-</html>
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+
+  const hoursElement = document.getElementById("hours");
+  const minutesElement = document.getElementById("minutes");
+  const secondsElement = document.getElementById("seconds");
+
+  hoursElement.textContent = hours;
+  minutesElement.textContent = minutes;
+  secondsElement.textContent = seconds;
+
+}
+
+
+setInterval(updateClock,1000);
